ade110f5dc82ff571cc3fb3e5270488f8f111809
Elton-Henrique-Oliveira <terezafe.oli@gmail.com>
2023-09-16 03:10:53 -0300

    introdução as exceções customizadas

---
+++ src/main/java/br/com/elton/exception/UnsupportedMathOperationException.java
@@ -0,0 +1,13 @@
+package br.com.elton.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class UnsupportedMathOperationException extends RuntimeException{
+    private static final long serialVersionUUID = 1L;
+
+    public UnsupportedMathOperationException(String exception) {
+        super(exception);
+    }
+}
